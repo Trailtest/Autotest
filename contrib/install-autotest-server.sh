e5ecb817418f7941f9c9136517c54a726e37488e
Deepika <gracedeepikapaul@gmail.com>
2018-06-06 09:58:46 +0530

    Changed URL

--- contrib/install-autotest-server.sh
+++ contrib/install-autotest-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ATHOME_DEFAULT=/usr/local/autotest
-AUTOTEST_DEFAULT_GIT_REPO='git://github.com/autotest/autotest.git'
+AUTOTEST_DEFAULT_GIT_REPO='git://github.com/Trailtest/Autotest.git'
 AUTOTEST_DEFAULT_GIT_BRANCH='master'
 DATETIMESTAMP=$(date "+%m-%d-%Y-%H-%M-%S")
 BASENAME=$(echo $(basename $0) | cut -f1 -d '.')
